b650e174f31bc899ad8bf43278a72823c76f7ed2
Antonio <antonio241710@gmail.com>
2020-03-20 17:33:47 +0100

    Introducción a Fetch y API de Coincap

--- src/components/PxIcon.vue
+++ src/components/PxIcon.vue
@@ -1,23 +1,23 @@
 <template>
-    <svg
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            width="40px"
-            viewBox="0 0 937 853"
-            enable-background="new 0 0 937 853"
-            xml:space="preserve"
-    >
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="40px"
+    viewBox="0 0 937 853"
+    enable-background="new 0 0 937 853"
+    xml:space="preserve"
+  >
     <image
-            id="image0"
-            width="937"
-            height="853"
-            x="0"
-            y="0"
-            xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA6kAAANVCAMAAACtUUjjAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+      id="image0"
+      width="937"
+      height="853"
+      x="0"
+      y="0"
+      xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA6kAAANVCAMAAACtUUjjAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
 AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAk1BMVEUAAABVVVVUVFRUVFRV
 VVVUVFRUVFRTU1NQUFBTU1NUVFRVVVVYWFhUVFRUVFRTU1NUVFR/f39paWmfn5+qqqq0tLTU1NT0
 9PT///+/v790dHTf39/q6urKysqUlJSJiYlfX19/dUWqlTWJfUFfXFBpZEy0nTHUtib/1hbfviK/
